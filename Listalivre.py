ca139def2f7b60bf6dc2d2afe2137115edec7040
Elaine Neto <elaine62211806@alunos.sc.senac.br>
2026-03-16 19:55:43 -0300

    Listalivre.py

---
+++ Listalivre.py
@@ -0,0 +1,8 @@
+lista_compras = ["leite", "pão", "ovos", "carne", "arroz" ]
+print("lista de compras:", lista_compras)
+
+lista_compras.append("frutas")
+print("Adicionando 'frutas':", lista_compras)
+
+lista_compras.remove("pão")
+print("Removendo 'pão':", lista_compras)
