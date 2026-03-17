f8f3587c4ca200225abbaaac3c5932a781d460d7
Elaine Neto <elaine62211806@alunos.sc.senac.br>
2026-03-16 21:08:09 -0300

    Tuplalivre.py

---
+++ Tuplalivre.py
@@ -0,0 +1,6 @@
+dias = ("segunda", "terça", "quarta", "quinta", "sexta", "sábado", "domingo")
+print("Dias da semana:", dias)
+
+print( "Primeiro dia:", dias[0]) 
+print("Último dia:", dias[-1])
+print("Dias de semana:", len(dias))
