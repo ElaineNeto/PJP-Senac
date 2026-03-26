6933b2a6585ff4f53eb3dcbf56fc0afe3e21a318
Elaine Neto <elaine62211806@alunos.sc.senac.br>
2026-03-25 21:05:35 -0300

    fp8.py

---
+++ fp8.py
@@ -0,0 +1,9 @@
+numero = input("Digite o número:")
+
+print("Valor lido", numero)
+print("Tipo antes", type(numero))
+
+numero = int(numero)
+
+print("Valor convertido:", numero)
+print("Tipo depois:", type(numero))
